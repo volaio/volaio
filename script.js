48d8feccce5be20f32dbcd1789e0d8930024e445
volaio <santiagogatti2404@gmail.com>
2025-08-03 23:34:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,17 +27,3 @@ function calcularPrecio() {
 
   document.getElementById('whatsappLink').href = linkWhatsApp;
 }
-
-// Botón scroll arriba
-window.onscroll = function () {
-  const btn = document.getElementById("scrollTopBtn");
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-};
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
